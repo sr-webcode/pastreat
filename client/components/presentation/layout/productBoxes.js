01491936189bf8050dea996219dafaeccb0ad387
srwebcode <sr.webcode@gmail.com>
2019-10-01 09:07:35 +0800

    fixed recent products section

--- client/components/presentation/layout/productBoxes.js
+++ client/components/presentation/layout/productBoxes.js
@@ -1,26 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const ProductBox = (props) => {
-
-  const itemsExtract = (val) => {
+const ProductBox = props => {
+  const itemsExtract = val => {
     return val.map((each, index) => {
-      return <div key={index} className="product-box-recent">
-        <div className="product-box-recent-img">
-          <img src="./images/assets/treat1.png" alt="cake" />
+      return (
+        <div key={index} className="product-box-recent">
+          <div className="product-box-recent-img">
+            <img src="./images/assets/cakeTemp.jpg" alt="" />
+          </div>
+
+          <div className="product-box-recent-caption">
+            <h3>{each.itemName}</h3>
+            <p>{each.desc}</p>
+          </div>
         </div>
-        <div className="product-box-recent-caption">
-          <h3>{each.itemName}</h3>
-          <p>{each.desc}</p>
-        </div>
-      </div>
-    })
-  }
+      );
+    });
+  };
 
   const { items } = props;
-  return (<Fragment>
-    {itemsExtract(items)}
-    <a href="#" className="cta-hero" >more products</a>
-  </Fragment>)
-}
+  return (
+    <Fragment>
+      {itemsExtract(items)}
+      <a href="#" className="cta-hero">
+        more products
+      </a>
+    </Fragment>
+  );
+};
 
 export default ProductBox;
