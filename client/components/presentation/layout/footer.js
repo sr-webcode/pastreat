9cb203a737f93de0495d0aedafcc2687e53110af
sean <sr.webcode@gmail.com>
2019-10-01 20:10:24 +0800

    need to arranged/fix subscribe section (design wise) , footer contents

--- client/components/presentation/layout/footer.js
+++ client/components/presentation/layout/footer.js
@@ -3,8 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      this is the footer!
-     </footer>
+      <div className="container">
+        <h2>hi mr.footer!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nam rerum aspernatur doloribus assumenda doloremque dolor libero at consectetur? Accusamus explicabo, perferendis veritatis laborum odio sunt blanditiis illo suscipit ullam ad et dolores recusandae aliquam voluptate, cum cumque animi harum quasi natus. In dolores ipsam dolorum vel adipisci veniam reiciendis!</p>
+      </div>
+
+    </footer>
   );
 };
 
--- client/components/presentation/layout/subscribeSection.js
+++ client/components/presentation/layout/subscribeSection.js
@@ -6,9 +6,6 @@ const SubscribeSection = () => {
     <section className="subscribe-section">
 
       <div className="container">
-        <div className="subscribe-chef">
-          <img src="./images/assets/chef.jpg" alt="" />
-        </div>
         <div className="subscribe-caption">
           <h2>We make delish pastries!</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo aliquid ratione animi dolorem quos cum recusandae doloremque nobis ex!</p>
