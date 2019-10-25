6bd6906fa85e81c4454f093bd7defe753e30c036
srwebcode <sr.webcode@gmail.com>
2019-10-25 12:53:54 +0800

    completed service section

--- client/components/presentation/routeContent/services.js
+++ client/components/presentation/routeContent/services.js
@@ -14,8 +14,59 @@ export default function Services() {
       </div>
 
       <div className="pastry-service-list">
+        <div className="container">
+          <div className="pastry-service-list-box">
+            <div className="pastry-service-image">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
 
-        
+            <div className="pastry-service-list-caption">
+              <h2>Birthdays</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consequuntur, accusantium earum excepturi est et id iusto illo
+                aliquid. Porro, quidem?
+              </p>
+            </div>
+            <div className="pastry-service-image-alt">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
+          </div>
+
+          <div className="pastry-service-list-box">
+            <div className="pastry-service-image">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
+            <div className="pastry-service-image-alt">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
+            <div className="pastry-service-list-caption">
+              <h2>Weddings</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consequuntur, accusantium earum excepturi est et id iusto illo
+                aliquid. Porro, quidem?
+              </p>
+            </div>
+          </div>
+
+          <div className="pastry-service-list-box">
+            <div className="pastry-service-image">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
+            <div className="pastry-service-list-caption">
+              <h2>Other Celebrations</h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consequuntur, accusantium earum excepturi est et id iusto illo
+                aliquid. Porro, quidem?
+              </p>
+            </div>
+            <div className="pastry-service-image-alt">
+              <img src="./images/assets/cater.jpg" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
